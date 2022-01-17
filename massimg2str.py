610b6c574ab9ccf53abd81a9d0e308a246c80d8a
NoraNeko0_0 <41070885+NoraNekoUwU@users.noreply.github.com>
2022-01-17 12:12:17 +0700

    update and make it more flexible

--- massimg2str.py
+++ massimg2str.py
@@ -6,18 +6,28 @@ import pytesseract
 import os
 
 os.system('figlet NoraNeko')
-print('Processing....')
-imgnum = 1
+print("Mass image to string tool")
+def img2str():
+	dirtxt = str(input("masukkan dir .txt kalian contoh : home/slamet/Documents/text.txt\n"))
+	dirimg = str(input("masukkan dir file image kalian contoh : home/slamet/Pictures/src/\n"))
+	typefile = str(input("masukkan type file contoh : jpeg/png\n"))
+	o = 1
+	p = int(input("Masukkan jumlah foto yang akan di scan!!\n"))
 
-while imgnum <= 17:
-##change y to your .txt directory to store the string
-	f = open("z","a")
-## change x to directory of your image source
-	x = 'x/%d.png'%(imgnum)
-	f.write(pytesseract.image_to_string(x))
-	f.close
-	imgnum += 1
-##put massimg2str.py in parent directory of your image source and change the image to ordered number and make a text.txt
-##in same directory as massimg2str.py
-##change the figlet to make it look cool lmao
-
+	while o <= p:
+		f = open("{}".format(dirtxt),"a")
+		x = '{}/{}.{}'.format(dirimg,o,typefile)
+		f.write(pytesseract.image_to_string(x))
+		f.close
+		print('Processing {}.{}....'.format(o,typefile))
+		o += 1
+	print("output file : {}".format(dirtxt))
+x = int(input("Start = 1\nExit = 2\n"))
+if x == 1 :
+	img2str()
+if x == 2 :
+	print("thanks for using!!")
+	exit()
+if x > 3 :
+	print("Don't be stupid")
+	exit()
